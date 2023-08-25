d7c528c03278d0042204389420c7105e3bb0166a
RukPhattaradol <rukphattaradol@gmail.com>
2023-08-25 20:35:29 +0700

    update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
